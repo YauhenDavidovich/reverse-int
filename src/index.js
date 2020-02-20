cbec15f7a6663040028426bf2088d8ee0388e595
Шурхай Алексей Сергеевич <a.shurkhai@ziko.loc>
2020-02-20 14:15:02 +0300

    feat: add task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 module.exports = function reverse (n) {
+    return parseFloat(String(n).split (""). reverse (). join ("")) //convert to str, then convert to array, then reverse array, then convert to str, then convert to number
   
 }
